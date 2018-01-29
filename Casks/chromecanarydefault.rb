2eecdb784809bfad0a4b6dbd1da83e8f102f25c7
Joel Baranick <joel.baranick@ensighten.com>
2018-01-29 10:34:13 -0800

    Intentionally broke hash

--- Casks/chromecanarydefault.rb
+++ Casks/chromecanarydefault.rb
@@ -1,6 +1,6 @@
 cask 'chromecanarydefault' do
   version "1.0"
-  sha256 '5c309d92e8f7e526359461709bab5944362f8e9a8f474a5f2d4c2cab260332e1'
+  sha256 'a'
 
   url "https://github.com/kadaan/homebrew-apps/releases/download/ChromeCanaryDefault_#{version}/ChromeCanaryDefault_#{version}.tbz2"
   name 'Chrome Canary'
