7e14f0e0516cc7a986ae92312f42847036e0abb6
Joel Baranick <joel.baranick@ensighten.com>
2018-01-29 10:23:32 -0800

    Update checksum

--- Casks/chromecanarydefault.rb
+++ Casks/chromecanarydefault.rb
@@ -1,6 +1,6 @@
 cask 'chromecanarydefault' do
   version "1.0"
-  sha256 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855'
+  sha256 '5c309d92e8f7e526359461709bab5944362f8e9a8f474a5f2d4c2cab260332e1'
 
   url "https://github.com/kadaan/homebrew-apps/releases/download/ChromeCanaryDefault_#{version}/ChromeCanaryDefault_#{version}.tbz2"
   name 'Chrome Canary'
